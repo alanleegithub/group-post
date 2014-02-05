9797ff4729b508ab0886716e66ca1088de825e42
alan.lee <alanlee2005@gmail.com>
2014-02-05 21:13:04 +0800

    using before_action to simple Group.find() function

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,11 @@
 class PostsController < ApplicationController
+    before_action :find_group
+
     def new
-        @group = Group.find(params[:group_id])
         @post = @group.posts.build
     end
     
     def create
-        @group=Group.find(params[:group_id])
         @post = @group.posts.new(post_params)
 
         if @post.save
@@ -16,14 +16,12 @@ class PostsController < ApplicationController
     end    
    
     def destroy
-        @group = Group.find(params[:group_id])
         @post = @group.posts.find(params[:id])
         @post.destroy
         redirect_to group_path(@group)
     end
     
      def update
-        @group = Group.find(params[:group_id])
         @post = @group.posts.find(params[:id])
         
         if @post.update(post_params)
@@ -34,7 +32,6 @@ class PostsController < ApplicationController
     end
     
     def edit
-        @group = Group.find(params[:group_id])
         @post = @group.posts.find(params[:id])
     end
    
@@ -44,4 +41,7 @@ class PostsController < ApplicationController
         params.require(:post).permit(:content)
     end
     
+    def find_group
+        @group = Group.find(params[:group_id])
+    end    
 end
